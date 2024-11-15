179932d4840fe4a82b0aa010b1859e2bef9c0fc4
holakirr <kpetunin@icloud.com>
2024-11-15 11:16:45 +0300

    feat: updates configs

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		dts({
-			// rollupTypes: true,
-		}),
 		react(),
 		tsconfigPaths(),
+		dts({
+			rollupTypes: true,
+		}),
 	],
 	build: {
 		lib: {
